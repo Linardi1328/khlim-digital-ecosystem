6701a2b1fa3f6d5790976ebacd8466932a612433
Richie Linardi <richb.linardi@gmail.com>
2026-08-30 02:29:50 +0800

    feat(i18n): merge enrolment translations

--- packages/i18n/src/translator.ts
+++ packages/i18n/src/translator.ts
@@ -7,6 +7,7 @@ import { hi } from "./messages/hi";
 import { webMessages } from "./messages/web";
 import { authWebMessages } from "./messages/auth-web";
 import { publicPageMessages } from "./messages/public-pages";
+import { enrolWebMessages } from "./messages/enrol-web";
 
 export const messages: Record<SupportedLocale, Record<string, string>> = {
   en: {
@@ -14,30 +15,35 @@ export const messages: Record<SupportedLocale, Record<string, string>> = {
     ...webMessages.en,
     ...authWebMessages.en,
     ...publicPageMessages.en,
+    ...enrolWebMessages.en,
   },
   ms: {
     ...ms,
     ...webMessages.ms,
     ...authWebMessages.ms,
     ...publicPageMessages.ms,
+    ...enrolWebMessages.ms,
   },
   "zh-Hans": {
     ...zhHans,
     ...webMessages["zh-Hans"],
     ...authWebMessages["zh-Hans"],
     ...publicPageMessages["zh-Hans"],
+    ...enrolWebMessages["zh-Hans"],
   },
   "zh-Hant": {
     ...zhHant,
     ...webMessages["zh-Hant"],
     ...authWebMessages["zh-Hant"],
     ...publicPageMessages["zh-Hant"],
+    ...enrolWebMessages["zh-Hant"],
   },
   hi: {
     ...hi,
     ...webMessages.hi,
     ...authWebMessages.hi,
     ...publicPageMessages.hi,
+    ...enrolWebMessages.hi,
   },
 };
 
