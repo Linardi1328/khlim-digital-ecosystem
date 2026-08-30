62cccf8eb83249d54f81feff0df0105a74728ba1
Richie Linardi <richb.linardi@gmail.com>
2026-08-30 13:39:39 +0800

    feat(admin): register staff access controller

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "../database/database.module";
+import { AdminAccessController } from "./admin-access.controller";
 import { AdminController } from "./admin.controller";
 import { AdminService } from "./admin.service";
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [AdminController],
+  controllers: [AdminAccessController, AdminController],
   providers: [AdminService],
 })
 export class AdminModule {}
